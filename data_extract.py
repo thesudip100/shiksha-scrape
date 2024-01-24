0d0ea7cba06bbf0edd4cfedb05c7def420a2ee53
Ishan Raj Sapkota <ishansapkota2@gmail.com>
2024-01-24 23:25:03 +0545

    trying to extract data from the main page

--- data_extract.py
+++ data_extract.py
@@ -1,81 +1,105 @@
-import json
+from urllib.parse import urljoin
 from playwright.sync_api import sync_playwright
+import json
 import time
-from bs4 import BeautifulSoup
-import requests
+#import requests
+#from requests import head
 
+main_url='https://www.shiksha.com'
 
-url = {}
-json_file = 'list.json'
-data_file = 'data.json'
-
-
-with open(json_file,'r') as file:
-    links = json.load(file)
-
-
+data_list = []
 data = {}
+
+output_file = 'data.json'
+
+def extractdata(page):
+    tplwrpr=page.query_selector('#tuplewrapper')
+    if tplwrpr:
+        x=tplwrpr.query_selector_all('._1822._0fc7._7efa')
+        print(len(x))
+    for i in x:
+        if i:
+            #for title
+            title_finder = i.query_selector('h3.f7cc')
+            title = title_finder.inner_text()
+            data['University Name'] = title
+
+            #for location
+            location_locator = i.query_selector('span._5588')
+            location = location_locator.inner_text()
+            data['Location'] = location
+
+            #for courses offered, tuition fee, scholarships
+            column_div = i.query_selector('.cd4f._5c64.contentColumn_3')
+            columns = column_div.query_selector_all('._77ff')
+            info = {}
+            for column in columns:
+                heading =column.query_selector('.abce')
+                information = column.query_selector('.dcfd.undefined')
+                if heading and information:
+                    info[f'{heading.inner_text()}']=information.inner_text()
+            data['Additional info']=info
+
+
+
+
+            print(data)
+
+                
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False,slow_mo=50)
     page = browser.new_page()
-    for link in links:
-        page.goto(link)
-        page.wait_for_selector("#main-wrapper")
-        time.sleep(2)
-        #to get the title of university
-        try:
-            titles_locator = page.query_selector(".e70a13")
-            titles = titles_locator.inner_text()
-            clean_title = titles.split(':')
-            if True:
-                data['University name'] = clean_title[0]
-        except: 
-            pass  
-        #to get the location of the university
-        try:
-             location_locator = page.query_selector("._7164e4.ece774.ece774")
-             location = location_locator.inner_text()
-             
-             if True:
-                  data['University_location'] = location
-        except:
-             pass
-        
-        #trying to get the accepted exams
-        # try:
-        #     page.mouse.wheel(0,500)
-        #     accepted_exams_button = page.query_selector("._7ed9")
-        #     if accepted_exams_button and accepted_exams_button.is_visible():
-        #          accepted_exams_button.click()
-        #          accepted_exam_div = page.query_selector("._17b3")
-        #          for i in accepted_exam_div:
-        #             accepted_exam = i.query_selector(".e3f3.ripple.dark")
-        #             clean_accepted_exam = accepted_exam.inner_text()
-        #             if True:
-        #                     data[f'accepted exams{i}'] = clean_accepted_exam
-        # except:
-        #      pass
-        
-        #to get scholarships
-        try:
-            page.mouse.wheel(0,700)
-            scholarship_locator = page.query_selector(".c221")
-            scholarship = scholarship_locator.get_attribute("a")
-            print(scholarship)
-            # if True:
-            #     data['scholarships'] = scholarship
-        except:
-            pass
-        
-    try:
-        data1 = {}
-        with open (data_file,'r') as f:
-            data1 = json.load(f)
-        with open(data_file,'w')as f:
-            data1 = {**data1,**data}
-            json.dump(data1,file,indent=3)
 
+    try:
+
+        page.goto('https://www.shiksha.com/studyabroad/australia/universities')
+
+        page.wait_for_selector('#main-wrapper')
+
+        tuple_wrapper=page.query_selector('#tuplewrapper')
+
+        time.sleep(7)
+
+
+        for i in range(1):
+            page.mouse.wheel(0, 500)
+            time.sleep(2)
+            extractdata(page)
+            try:
+                lazy_button=tuple_wrapper.query_selector('#lazy_load_next_4')
+                button = lazy_button.query_selector('#lazy_load_next_btn_4')
+                if button and button.is_visible():
+                    button.click()
+                    page.mouse.wheel(0,-5000)
+                extractdata(page)
+            except:
+                continue
+            
+                
     except Exception as e:
-        with open(data_file,'w')as f: 
-            print('No value',e)
-            json.dump(data,f,indent=3)
+        print(e)
+
+
+        time.sleep(3)
+
+        browser.close()
+    except Exception as ex:
+        print(ex)
+
+    # try: 
+    #     url={}
+    #     with open(output_file,'r') as file:
+    #         url = json.load(file)
+        
+    #     with open(output_file,'w') as file:
+    #         url = {**url,**unique_urls}
+    #         json.dump(url,file,indent=3)
+
+    # except Exception as e:
+    #     print("Error opening file ",e)
+    #     with open(output_file,'w') as file:
+    #      json.dump(unique_urls,file,indent = 3)
+
+
+    # Close the browser
+    browser.close()
